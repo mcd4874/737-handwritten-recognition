439ea666d52208c3c404bac9676d84f3bf754186
william duong <williamduong@williams-MacBook-Pro.local>
2019-03-25 16:46:56 -0400

    got code to generate classifier using KD tree

--- experiments.py
+++ experiments.py
@@ -1,7 +1,10 @@
 from sklearn.utils import resample
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KDTree
+from sklearn.metrics import confusion_matrix,classification_report
 from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -15,8 +18,8 @@ def stackImages(imagesPath, sampleListFile):
     stack = None
 
     # Iterate over the lines in the sampleList file
-    # sampleSize = len(data)
-    sampleSize = 10
+    sampleSize = len(data)
+    # sampleSize = 100
     # for i in range(len(data)):
     targetClasses = list()
     for i in range(sampleSize):
@@ -27,13 +30,13 @@ def stackImages(imagesPath, sampleListFile):
         elements = str(data[i][0]).split("_")
         id = elements[len(elements) - 1]
         filename = imagesPath + id + ".png"
-        print("filename=",  filename)
+        # print("filename=",  filename)
         img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
         #print("img =", img)
         #print("img type =", type(img))
 
         imgRavel = img.ravel()
-        print(imgRavel.shape)
+        # print(imgRavel.shape)
         #print("imgRavel = ", imgRavel, " shape is ", imgRavel.shape)
 
         # Lazily allocate the stack array
@@ -52,16 +55,24 @@ def stackImages(imagesPath, sampleListFile):
     return stack,targetClasses
 
 def trainKDTreeClassifier(stack,targetClasses):
-    print("About to run KDTree")
-    kdtree = KDTree(stack)
-    print("About to query KDTree")
-    dist, ind = kdtree.query(stack[1], k=3)
-    print("indices of 3 closest = ", ind)
-    print("distances of 3 closest = ", dist)
+    # print("About to run KDTree")
+    # kdtree = KDTree(stack)
+    # print("About to query KDTree")
+    # dist, ind = kdtree.query(stack[1], k=3)
+    # print("indices of 3 closest = ", ind)
+    # print("distances of 3 closest = ", dist)
 
-    return
+    kdTree = KNeighborsClassifier(n_neighbors=1, algorithm='kd_tree')
+    kdTree.fit(stack,targetClasses)
 
-def testKDTreeClassifier(testSamplesFile, kdtree):
+    return kdTree
+
+def testKDTreeClassifier(testSamplesFile, testTargetSample, kdtree, encoderModel):
+    predict = kdtree.predict(testSamplesFile)
+    print("prediction: ",predict)
+    print("actual label : ",testSamplesFile)
+    print(confusion_matrix(testSamplesFile, predict, labels=encoderModel.classes_))
+    print(classification_report(testSamplesFile, predict, target_names=encoderModel.classes_))
     return
 
 def generateLabelsEncoder(targetClasses):
@@ -77,10 +88,19 @@ def inverseTransformLabels(targetClasses,encoderModel):
 
 def main():
     trainSymbols,targetSymbols = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_train_resampled.txt")
-    #testSymbols = stackImages("./images/connect/junk/", "./trainingSymbols/iso_GT_test_resampled.txt")
+    testSymbols,testTargetSymbols = stackImages("./images/connect/junk/", "./trainingSymbols/iso_GT_test_resampled.txt")
 
-    # kdtree = trainKDTreeClassifier(trainSymbols,targetSymbols)
-    #testKDTreeClassifier(testSymbols, kdtree)
+    encoderModel = generateLabelsEncoder(targetSymbols)
+    # encoderModel = generateLabelsEncoder(testTargetSymbols)
+
+    print(encoderModel.classes_)
+    labelTrainTarget = transformLabels(targetSymbols,encoderModel)
+    labelTestTarget = transformLabels(testTargetSymbols,encoderModel)
+    print("train target: ",labelTrainTarget)
+    print("test target: ",labelTestTarget)
+    #
+    kdtree = trainKDTreeClassifier(trainSymbols,labelTrainTarget)
+    testKDTreeClassifier(testSymbols,labelTestTarget, kdtree,encoderModel)
 
     return
 
