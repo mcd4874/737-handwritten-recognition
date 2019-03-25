96d0478e4b42bbcb3a470abad7a28aa946776ff5
william duong <williamduong@williams-MacBook-Pro.local>
2019-03-25 13:42:54 -0400

    provide function to generate labels encoder for training

--- experiments.py
+++ experiments.py
@@ -1,6 +1,7 @@
 from sklearn.utils import resample
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KDTree
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -14,10 +15,14 @@ def stackImages(imagesPath, sampleListFile):
     stack = None
 
     # Iterate over the lines in the sampleList file
-    for i in range(len(data)):
+    # sampleSize = len(data)
+    sampleSize = 10
+    # for i in range(len(data)):
+    targetClasses = list()
+    for i in range(sampleSize):
         # Extract the target class
         target = data[i][1]
-
+        targetClasses.append(target)
         # Extract the unique identifier for the symbol
         elements = str(data[i][0]).split("_")
         id = elements[len(elements) - 1]
@@ -28,6 +33,7 @@ def stackImages(imagesPath, sampleListFile):
         #print("img type =", type(img))
 
         imgRavel = img.ravel()
+        print(imgRavel.shape)
         #print("imgRavel = ", imgRavel, " shape is ", imgRavel.shape)
 
         # Lazily allocate the stack array
@@ -36,14 +42,16 @@ def stackImages(imagesPath, sampleListFile):
             width = img.shape[1]
             #print("height and width = ", height, " and ", width)
             #print("length = ", len(data))
-            stack = np.zeros((len(data), height*width))
+            stack = np.zeros((sampleSize, height*width))
 
         # Merge this flattened image into our stack
         stack[i] = imgRavel
+    targetClasses = np.array(targetClasses, dtype=np.dtype('a16'))
+    # print(targetClasses.shape)
+    # print(stack.shape)
+    return stack,targetClasses
 
-    return stack
-
-def trainKDTreeClassifier(stack):
+def trainKDTreeClassifier(stack,targetClasses):
     print("About to run KDTree")
     kdtree = KDTree(stack)
     print("About to query KDTree")
@@ -56,11 +64,22 @@ def trainKDTreeClassifier(stack):
 def testKDTreeClassifier(testSamplesFile, kdtree):
     return
 
+def generateLabelsEncoder(targetClasses):
+    enc = LabelEncoder()
+    enc.fit(targetClasses)
+    return enc
+
+def transformLabels(targetClasses,encoderModel):
+    return encoderModel.transform(targetClasses)
+
+def inverseTransformLabels(targetClasses,encoderModel):
+    return encoderModel.inverse_transform(targetClasses)
+
 def main():
-    trainSymbols = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_train_resampled.txt")
+    trainSymbols,targetSymbols = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_train_resampled.txt")
     #testSymbols = stackImages("./images/connect/junk/", "./trainingSymbols/iso_GT_test_resampled.txt")
 
-    kdtree = trainKDTreeClassifier(trainSymbols)
+    # kdtree = trainKDTreeClassifier(trainSymbols,targetSymbols)
     #testKDTreeClassifier(testSymbols, kdtree)
 
     return
