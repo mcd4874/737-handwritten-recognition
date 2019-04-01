2b1adcaf5b0fd752cd6ee0429437ddc6899de4b4
william duong <williamduong@williams-MacBook-Pro.local>
2019-03-31 20:33:40 -0400

    can generate the table file

--- experiments.py
+++ experiments.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix,classification_report
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils.multiclass import unique_labels
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ import math
 import xml.etree.ElementTree as ET
 import os
 import cv2
-
+from yellowbrick.classifier import ConfusionMatrix
 def stackImages(imagesPath, sampleListFile):
     # Load the dictionary file
     data = np.genfromtxt(sampleListFile, delimiter=',', dtype=str)
@@ -24,12 +25,14 @@ def stackImages(imagesPath, sampleListFile):
     # sampleSize = 100
     # for i in range(len(data)):
     targetClasses = list()
+    identifier = list()
     for i in range(sampleSize):
         # Extract the target class
         target = data[i][1]
         targetClasses.append(target)
         # Extract the unique identifier for the symbol
         elements = str(data[i][0]).split("_")
+        identifier.append(str(data[i][0]))
         id = elements[len(elements) - 1]
         filename = imagesPath + id + ".png"
         # print("filename=",  filename)
@@ -51,10 +54,11 @@ def stackImages(imagesPath, sampleListFile):
 
         # Merge this flattened image into our stack
         stack[i] = imgRavel
+    identifier = np.array(identifier, dtype=np.dtype('a16'))
     targetClasses = np.array(targetClasses, dtype=np.dtype('a16'))
     # print(targetClasses.shape)
     # print(stack.shape)
-    return stack,targetClasses
+    return stack,targetClasses,identifier
 
 def trainKDTreeClassifier(stack,targetClasses):
     # print("About to run KDTree")
@@ -134,32 +138,104 @@ def loadModel(pkl_filename):
         print("finish load model from ",pkl_filename)
         return pickle_model
 
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
 
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+import pandas as pd
 def main():
-    # trainSymbols,targetSymbols = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_train.txt")
-    testSymbols,testTargetSymbols = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_test.txt")
+    trainSymbols,targetSymbols,trainIdentifiers = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_train.txt")
+    testSymbols,testTargetSymbols,testIdentifiers = stackImages("./images/connect/symbols/", "./trainingSymbols/iso_GT_test.txt")
 
-    # encoderModel = generateLabelsEncoder(targetSymbols)
-    # saveModel("encoder.pkl",encoderModel)
+    print(testIdentifiers.shape)
+    print(testIdentifiers[0])
+    print(np.isnan(testSymbols).any())
+    encoderModel = generateLabelsEncoder(targetSymbols)
+    saveModel("encoder.pkl",encoderModel)
 
-    encoderModel = loadModel("encoder.pkl")
+    # encoderModel = loadModel("encoder.pkl")
 
     # trainSymbols = trainSymbols[:20000]
     # targetSymbols = targetSymbols[:20000]
-    testSymbols = testSymbols[:1000]
-    testTargetSymbols = testTargetSymbols[:1000]
-    # labelTrainTarget = transformLabels(targetSymbols,encoderModel)
+    # testSymbols = testSymbols[:1000]
+    # testTargetSymbols = testTargetSymbols[:1000]
+    labelTrainTarget = transformLabels(targetSymbols,encoderModel)
     labelTestTarget = transformLabels(testTargetSymbols,encoderModel)
 
     # kdtree = trainKDTreeClassifier(trainSymbols,labelTrainTarget)
     # saveModel("result.pkl",kdtree)
     kdtree = loadModel("result.pkl")
     list_test_best_label_predict = get_list_indices_predict(testSymbols,labelTestTarget, kdtree,encoderModel)
+    # print(list_test_best_label_predict.shape)
+    #
+    df1 = pd.DataFrame(testIdentifiers)
+    df2 = pd.DataFrame(list_test_best_label_predict)
+    r = pd.concat([df1,df2],axis=1)
+    r.to_csv("report_table.csv",index = False,header = False)
 
-
-
-
+    # Plot non-normalized confusion matrix
+    # plot_confusion_matrix(kdtree.predict(testSymbols), labelTestTarget, classes=encoderModel.classes_,
+    #                       title='Confusion matrix, without normalization')
+    # plt.show()
+    # iris_cm = ConfusionMatrix(
+    #     kdtree, classes=encoderModel.classes_
+    #
+    # )
+    #
+    # iris_cm.fit(trainSymbols, labelTrainTarget)
+    # iris_cm.score(testSymbols, labelTestTarget)
+    # #
+    # iris_cm.poof()
+    # plt.show()
 
     # testKDTreeClassifier(testSymbols,labelTestTarget, kdtree,encoderModel)
     # maxTrees = 100
